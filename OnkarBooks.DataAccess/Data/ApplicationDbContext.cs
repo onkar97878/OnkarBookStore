cc9eed326ea0ccbd2daeab6c0f4815ac6f4e6dfc
W0834099 <W0834099@myscc.ca>
2024-03-24 16:39:09 -0400

    addingthemigrationViaPmConsole.Rerun-themigrationsandside-bysideUpdateTheDATABASSE

--- OnkarBooks.DataAccess/Data/ApplicationDbContext.cs
+++ OnkarBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using OnkarBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace OnkarBookStore.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
