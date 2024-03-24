9092a0638052f589098e6b1764561e670ea9e8f6
W0834099 <W0834099@myscc.ca>
2024-03-24 15:25:08 -0400

    CopyAndPasteTheDataFolderIntoThreeNewprojectsAndDoingSomechangesByGivenInstructions

---
+++ OnkarBooks.Utility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnkarBooks.Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
