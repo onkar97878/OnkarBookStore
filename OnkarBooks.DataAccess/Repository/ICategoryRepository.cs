b711a133b9cefaf3a43a33f1903d13ed9021a69f
W0834099 <W0834099@myscc.ca>
2024-03-24 16:53:03 -0400

    ADDINGSOMEITEMSINrEPOSITORYaNDiREPOSITORYfOLDER

---
+++ OnkarBooks.DataAccess/Repository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using OnkarBooks.DataAccess.Repository.iRepository;
+using OnkarBooks.Models;
+
+namespace OnkarBooks.DataAccess.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
