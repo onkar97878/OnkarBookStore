17248f0338d8e1d28667986b032d8ef6432d3dca
W0834099 <W0834099@myscc.ca>
2024-03-24 15:59:37 -0400

    ReviewTheErrors

--- OnkarBookStore/Controllers/HomeController.cs
+++ OnkarBookStore/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OnkarBookStore.Models;
+using OnkarBookStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
